997cb260cfe2889198786e664c8e73121654c726
raphael.attias <raphael.attias@laplateforme.io>
2023-12-04 14:49:19 +0100

    pour aller encore plus loin fait

--- mainpy.py
+++ mainpy.py
@@ -1,5 +1,7 @@
 import hashlib
 import json
+import random
+import string
 
 def charger_mots_de_passe(chemin):
     try:
@@ -14,43 +16,69 @@ def sauvegarder_mots_de_passe(chemin, mots_de_passe):
     with open(chemin, 'w') as fichier_json:
         json.dump(data, fichier_json)
 
+def generer_mot_de_passe():
+    caracteres = string.ascii_letters + string.digits + string.punctuation
+    return ''.join(random.choice(caracteres) for _ in range(12))
+
+def afficher_mot_de_passe(libelle):
+    mots_de_passe_existants = charger_mots_de_passe("base.json")
+    mot_de_passe = mots_de_passe_existants.get(libelle)
+    if mot_de_passe:
+        print(f"Mot de passe pour '{libelle}' : {mot_de_passe}")
+    else:
+        print(f"Aucun mot de passe trouvé pour le libellé '{libelle}'.")
+
 def ajouter_mot_de_passe():
     libelle = input("Libellé du mot de passe : ")
+    
+    # Vérifier si le mot de passe existe déjà
+    mots_de_passe_existants = charger_mots_de_passe("base.json")
+    if libelle in mots_de_passe_existants:
+        print("Un mot de passe avec ce libellé existe déjà.")
+        return
+
     a = 0
     while a < 4:
-        pwd = input("Entrez votre mot de passe : ")
-        
-        if len(pwd) < 8:
-            print("Mot de passe trop court")
+        choix_generer = input("Voulez-vous générer un mot de passe aléatoire ? (O/N) : ")
+        if choix_generer.upper() == "O":
+            pwd = generer_mot_de_passe()
+            print(f"Mot de passe généré : {pwd}")
+            break
+        elif choix_generer.upper() == "N":
+            pwd = input("Entrez votre mot de passe : ")
+            break
         else:
-            a += 1
-        
-        if any(char in pwd for char in "!@#$%^&*"):
-            a += 1
-        else:
-            print("Aucun caractère spécial trouvé")
+            print("Choix invalide, veuillez répondre par O ou N.")
 
-        if any(char.isupper() for char in pwd):
-            a += 1
-        else:
-            print("Au moins une lettre majuscule est requise")
+    if len(pwd) < 8:
+        print("Mot de passe trop court")
+        return
 
-        if any(char.islower() for char in pwd):
-            a += 1
-        else:
-            print("Au moins une lettre minuscule est requise")
+    if any(char in pwd for char in "!@#$%^&*"):
+        a += 1
+    else:
+        print("Aucun caractère spécial trouvé")
 
-        if any(char.isalpha() or char.isdigit() for char in pwd):
-            a += 1
-        else:
-            print("Le mot de passe ne peut contenir que des lettres et des chiffres")
-    
+    if any(char.isupper() for char in pwd):
+        a += 1
+    else:
+        print("Au moins une lettre majuscule est requise")
+
+    if any(char.islower() for char in pwd):
+        a += 1
+    else:
+        print("Au moins une lettre minuscule est requise")
+
+    if any(char.isalpha() or char.isdigit() for char in pwd):
+        a += 1
+    else:
+        print("Le mot de passe ne peut contenir que des lettres et des chiffres")
+
     print("Mot de passe est bien Safe")
     # Hash du mot de passe
     h = hashlib.sha256(pwd.encode('UTF-8'))
     hashed_pwd = h.hexdigest()
     chemin_mots_de_passe = "base.json"
-    mots_de_passe_existants = charger_mots_de_passe(chemin_mots_de_passe)
     mots_de_passe_existants[libelle] = hashed_pwd
     sauvegarder_mots_de_passe(chemin_mots_de_passe, mots_de_passe_existants)
     print("Mot de passe ajouté avec succès!")
@@ -77,7 +105,7 @@ while True:
     
     elif choix == "2":
         libelle = input("Entrez le libellé du mot de passe que vous souhaitez afficher : ")
-        charger_mots_de_passe(libelle) 
+        afficher_mot_de_passe(libelle)
 
     elif choix.lower() == "q":
         break
